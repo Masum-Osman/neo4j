7686c25f693fb260c8a3a2e5ca4c434067b8e7e7
Masum_Osman <ogkmasum@gmail.com>
2022-10-13 13:30:35 +0600

    trying to do SET command

---
+++ go.mod
@@ -0,0 +1,5 @@
+module neo4j
+
+go 1.18
+
+require github.com/neo4j/neo4j-go-driver/v5 v5.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
+)
+
+func main() {
+	dbUri := "neo4j://localhost:7687"
+	driver, err := neo4j.NewDriver(dbUri, neo4j.BasicAuth("neo4j", "102080", ""))
+	if err != nil {
+		panic(err)
+	}
+	defer driver.Close()
+
+	item, err := insertItem(driver)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%v\n", item)
+}
+
+// func setItem(driver neo4j.Driver)
+
+func insertItem(driver neo4j.Driver) (*Item, error) {
+	session := driver.NewSession(neo4j.SessionConfig{})
+	defer session.Close()
+
+	result, err := session.WriteTransaction(createItemFn)
+
+	if err != nil {
+		return nil, err
+	}
+	return result.(*Item), nil
+}
+
+func createItemFn(tx neo4j.Transaction) (interface{}, error) {
+	records, err := tx.Run(`
+		match (n:Item{name: "Item 1"})
+		set n.surname = "anupom"
+		return n`,
+		map[string]interface{}{
+			"id":   2,
+			"name": "Item 2",
+		})
+	if err != nil {
+		return nil, err
+	}
+
+	record, err := records.Single()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Item{
+		Id:   record.Values[0].(int64),
+		Name: record.Values[1].(string),
+	}, nil
+}
+
+type Item struct {
+	Id   int64
+	Name string
+}
